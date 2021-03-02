4f37734b18f249d822a789b2a9932f5377ae26b7
Faustas Anulis <45820787+faustis1337@users.noreply.github.com>
2021-03-02 02:42:15 +0100

    .

--- src/rps/gui/controller/StartController.java
+++ src/rps/gui/controller/StartController.java
@@ -19,7 +19,8 @@ public class StartController {
             try {
                 Parent root = FXMLLoader.load(getClass().getResource("../view/GameView.fxml"));
                 Stage stage = (Stage) ((Node) ae.getSource()).getScene().getWindow();
-                stage.setScene(new Scene(root));
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
             } catch (Exception e) {
                 e.printStackTrace();
             }
